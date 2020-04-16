11adbe569e8eb92b20c36a6999d447331da327d2
Anton <akrevetka@gmail.com>
2020-04-16 13:59:07 +0700

    use ngrok

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = '12345'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'ddb6a902.ngrok.io',
+    'localhost',
+    '127.0.0.1'
+]
 
 
 # Application definition
